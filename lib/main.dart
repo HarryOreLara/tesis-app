8d711905148de8898d5b170cdba98d5292cafd1f
Harry Ore Lara <harryorelara@gmail.com>
2023-11-08 17:38:02 -0500

    Configurando main

--- lib/main.dart
+++ lib/main.dart
@@ -4,27 +4,18 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tesis_app/config/router/app_router.dart';
 import 'package:tesis_app/config/theme/app_theme.dart';
 import 'package:tesis_app/core/dependencies/app_dependencies.dart';
-import 'package:tesis_app/infraestructure/auth/auth_service.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  final authService = AuthService();
-  final idPersonaNullable = await authService.getUserId();
-  final idPersona = idPersonaNullable ?? "";
-
-  runApp(ProviderScope(
-      child: idPersona.isEmpty ? LoginApp(authService) : HomeApp(authService)));
+void main() {
+  runApp(const ProviderScope(child: MyApp()));
 }
 
-class LoginApp extends StatelessWidget {
-  final AuthService authService;
-
-  const LoginApp(this.authService, {super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
-      providers: AppDependencies.blocProviders, // Agrega tus BlocProviders
+      providers: AppDependencies.blocProviders,
       child: MaterialApp.router(
         routerConfig: appRouter,
         debugShowCheckedModeBanner: false,
@@ -33,37 +24,3 @@ class LoginApp extends StatelessWidget {
     );
   }
 }
-
-class HomeApp extends StatelessWidget {
-  final AuthService authService;
-
-  const HomeApp(this.authService, {super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: AppDependencies.blocProviders, // Agrega tus BlocProviders
-      child: MaterialApp.router(
-        routerConfig: appRouter,
-        debugShowCheckedModeBanner: false,
-        theme: AppTheme().getTheme(),
-      ),
-    );
-  }
-}
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MultiBlocProvider(
-//       providers: AppDependencies.blocProviders,
-//       child: MaterialApp.router(
-//         routerConfig: appRouter,
-//         debugShowCheckedModeBanner: false,
-//         theme: AppTheme().getTheme(),
-//       ),
-//     );
-//   }
-// }
