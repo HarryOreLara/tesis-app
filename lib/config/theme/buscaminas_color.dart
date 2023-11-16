bfc71333f4ff9d68126fd10a19af4c53bda87f6a
Harry Ore Lara <harryorelara@gmail.com>
2023-11-16 17:47:27 -0500

    Terminando el minijuego buscamina

---
+++ lib/config/theme/buscaminas_color.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class BuscaminasColor {
+  static Color primaryColor = const Color(0xFF1f1b18);
+  static Color secondaryColor = const Color(0xFFdd6e41);
+  static Color accentColor = const Color(0xFF7a9eb8);
+  static Color lightPrimaryColor = const Color(0xFF3f3a36);
+  static Color clickedCard = const Color(0xFF818181);
+
+  static List<Color> letterColors = [
+    Colors.white,
+    Colors.blue.shade100,
+    Colors.greenAccent,
+    Colors.green,
+    Colors.yellow,
+    Colors.amber,
+    Colors.orange,
+    Colors.red
+  ];
+}
--- lib/presentation/screens/entertainment/minigames_list/buscaminas.dart
+++ lib/presentation/screens/entertainment/minigames_list/buscaminas.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:tesis_app/config/theme/buscaminas_color.dart';
+import 'package:tesis_app/core/utils/buscaminas.dart';
 
 class BuscaminasScreen extends StatefulWidget {
   const BuscaminasScreen({super.key});
@@ -8,12 +11,185 @@ class BuscaminasScreen extends StatefulWidget {
 }
 
 class _BuscaminasScreenState extends State<BuscaminasScreen> {
+  Buscaminas buscaminas = Buscaminas();
+
+  @override
+  void initState() {
+    super.initState();
+    buscaminas.generateMap();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: BuscaminasColor.primaryColor,
       appBar: AppBar(
-        title: const Text("Buscaminas"),
+        title: const Text(
+          "Buscaminas",
+          style: TextStyle(color: Colors.white),
+        ),
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.settings,
+                color: Colors.white,
+              ))
+        ],
+        leading: IconButton(
+            onPressed: () {
+              context.pop(context);
+            },
+            icon: const Icon(
+              Icons.arrow_back_ios,
+              color: Colors.white,
+            )),
+        backgroundColor: BuscaminasColor.primaryColor,
         centerTitle: true,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 20.0),
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 22.0, vertical: 16.0),
+                  decoration: BoxDecoration(
+                      color: BuscaminasColor.lightPrimaryColor,
+                      borderRadius: BorderRadius.circular(8.0)),
+                  child: const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Icon(
+                        Icons.flag,
+                        color: Color(0xFF7a9eb8),
+                        size: 34.0,
+                      ),
+                      Text(
+                        "10",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 32.0,
+                            fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 20.0),
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 22.0, vertical: 16.0),
+                  decoration: BoxDecoration(
+                      color: BuscaminasColor.lightPrimaryColor,
+                      borderRadius: BorderRadius.circular(8.0)),
+                  child: const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Icon(
+                        Icons.timer,
+                        color: Color(0xFF7a9eb8),
+                        size: 34.0,
+                      ),
+                      Text(
+                        "10:32",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 32.0,
+                            fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+          Container(
+            width: double.infinity,
+            height: 500.0,
+            padding: const EdgeInsets.all(20.0),
+            child: GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: Buscaminas.row,
+                  crossAxisSpacing: 4.0,
+                  mainAxisSpacing: 4.0),
+              itemCount: Buscaminas.cells,
+              itemBuilder: (context, index) {
+                Color cellColor = buscaminas.gameMap[index].reveal
+                    ? BuscaminasColor.clickedCard
+                    : BuscaminasColor.lightPrimaryColor;
+                return GestureDetector(
+                  onTap: buscaminas.gameOver
+                      ? null
+                      : () {
+                          setState(() {
+                            buscaminas
+                                .getClickedCell(buscaminas.gameMap[index]);
+                          });
+                        },
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: cellColor,
+                        borderRadius: BorderRadius.circular(6.0)),
+                    child: Center(
+                      child: Text(
+                        buscaminas.gameMap[index].reveal
+                            ? "${buscaminas.gameMap[index].content}"
+                            : "",
+                        style: TextStyle(
+                            color: buscaminas.gameMap[index].reveal
+                                ? buscaminas.gameMap[index].content == "X"
+                                    ? Colors.red
+                                    : BuscaminasColor.letterColors[
+                                        buscaminas.gameMap[index].content]
+                                : Colors.transparent,
+                            fontSize: 20.0),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          Text(
+            buscaminas.gameOver ? "Perdiste" : "",
+            style: const TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 32.0),
+          ),
+          const SizedBox(
+            height: 20.0,
+          ),
+          RawMaterialButton(
+            onPressed: () {
+              setState(() {
+                buscaminas.resetGame();
+                buscaminas.gameOver = false;
+              });
+            },
+            fillColor: BuscaminasColor.lightPrimaryColor,
+            elevation: 0,
+            shape: const StadiumBorder(),
+            padding:
+                const EdgeInsets.symmetric(horizontal: 64.0, vertical: 20.0),
+            child: const Text(
+              "Volver a intentar",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+          SizedBox(
+            height: 20.0,
+          )
+        ],
       ),
     );
   }
