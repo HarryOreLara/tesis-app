46304ceca2e3a0915f2d690b1be4619919b12ff4
Harry Ore Lara <harryorelara@gmail.com>
2024-02-05 15:15:52 -0500

    Agegando el archivo de entorno

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:tesis_app/config/constants/enviroment.dart';
 import 'package:tesis_app/config/router/app_router.dart';
 import 'package:tesis_app/config/theme/app_theme.dart';
 import 'package:tesis_app/core/dependencies/app_dependencies.dart';
 
-void main() {
+void main() async {
+  await Enviroment.initEnviroment();
   runApp(const ProviderScope(child: MyApp()));
 }
 
